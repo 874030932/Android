3209c3f3feb197ca7e16c737315d765d01a9d0ce
RanXusong <874030932@qq.com>
2018-12-19 21:09:42 +0800

    seconde commit

--- app/src/main/java/com/example/dell/finalproject/MainActivity.java
+++ app/src/main/java/com/example/dell/finalproject/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // do you like it????？？？？？？？？？/
         //my name is phw
+        // wo shi ni baba
     }
 }
